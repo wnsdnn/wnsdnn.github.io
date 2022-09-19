36c65e71754e52103138f077eb6d239bc62f53f1
wnsdnn <a24369029@gmail.com>
2022-09-19 12:35:03 +0900

    이번에야 말로?

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -14,7 +14,11 @@ export default {
                 src: 'https://www.googletagmanager.com/gtag/js?id=G-YKBFVJC3YF',
             },
         ],
-        ['script', {}, ["window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-YKBFVJC3YF');",],],
+        [
+            'script',
+            {},
+            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-YKBFVJC3YF');"
+        ]
         ['meta', { name: 'google-site-verification', content: 'zhv4UHG7Qfn8At3XKNXpBEVVNDHA3hinj_avfyq4m_A' }],
     ],
     srcDir : mdDir,
