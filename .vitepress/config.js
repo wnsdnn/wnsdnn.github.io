f4ffe16647fd3de732740b87d58fa6b942bc9ef6
wnsdnn <a24369029@gmail.com>
2022-11-03 22:16:16 +0900

    11_03

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -65,6 +65,7 @@ function getSidebar(){
                 items: [
                     { text: '11월 01일', link: '/sdhs/2211/221101' },
                     { text: '11월 02일', link: '/sdhs/2211/221102' },
+                    { text: '11월 03일', link: '/sdhs/2211/221103' },
                 ]
             },
         ],
